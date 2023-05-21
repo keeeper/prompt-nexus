d763aec91e91025181986e31b3e8f35d9cd2b6a7
keeeper <keeper96@gmail.com>
2023-05-22 00:05:06 +0200

    tags, avatar, username clickable areas fix. content shift when scroll appears fix

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -21,14 +21,13 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
     if (post.creator._id === session?.user.id) return router.push("/profile");
     router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
   }
-
   return (
     <div className="prompt-card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
-          <Image src={post.creator.image} width={40} height={40} alt="User avatar" className="rounded-full object-contain"/>
+        <div className="flex justify-start items-center gap-3 cursor-pointer" onClick={handleNameClick}>
+          <Image src={post.creator.image} width={40} height={40} alt="User avatar" className="rounded-full object-contain" />
           <div className="flex flex-col">
-            <h3 className="font-wix font-semibold text-gray-900" onClick={handleNameClick}>{post.creator.username}</h3>
+            <h3 className="font-wix font-semibold text-gray-900">{post.creator.username}</h3>
           </div>
         </div>
         <div className="btn-copy" onClick={handleCopy}>
@@ -36,7 +35,11 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
         </div>
       </div>
       <p className="my-4 text-sm font-wix text-gray-700">{post.prompt}</p>
-      <p className="font-inter text-sm cursor-pointer text-dark" onClick={()=> handleTagClick && handleTagClick(post.tag)}>#{post.tag}</p>
+      <a 
+        className={`font-inter text-sm cursor-pointer text-dark ${pathName.includes("/profile") ? 'pointer-events-none' : ''}`} 
+        onClick={()=> handleTagClick && handleTagClick(post.tag)}>
+          #{post.tag}
+      </a>
       {session?.user.id === post.creator._id && pathName === "/profile" && (
         <div className="flex-between gap-4 mt-5 pt-3 border-t border-gray-300 ">
           <button type="button" className="btn-link text-gray-600" onClick={handleEdit}>Edit</button>
